91cddc6b6d09ae7644c96919d22ba399f47586bb
russe <russe@10.153.159.1>
2026-05-30 16:24:20 -0600

    arreglos en el pom

--- src/main/java/Controlador/MotorJuego.java
+++ src/main/java/Controlador/MotorJuego.java
@@ -1,6 +1,15 @@
 package Controlador;
 
+import java.io.IOException;
+import javafx.fxml.FXML;
+import proyectofinaldesarrolloIII.App;
+
 
 public class MotorJuego {
+    
+     @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
 
 }
