db6237f972ee1cfa562acbaf5598811934872646
sronn <sronn@Amoaminovia>
2026-05-30 19:26:16 -0600

    VistaInicio

---
+++ src/main/java/Controlador/VistaInicioController.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author sronn
+ */
+public class VistaInicioController {
+
+    @FXML
+    private Button btnAdmin;
+
+    @FXML
+    private Button btnIniciarSesion;
+
+    @FXML
+    private Button btnRegistrarUsuario;
+
+    @FXML
+    private TabPane tabLogin;
+
+    @FXML
+    private PasswordField txtConfirmarContrasena;
+
+    @FXML
+    private PasswordField txtContrasenaIniciarSesion;
+
+    @FXML
+    private TextField txtRegistarUsuario;
+
+    @FXML
+    private PasswordField txtRegistrarContrasena;
+
+    @FXML
+    private TextField txtRegistrarCorreo;
+
+    @FXML
+    private TextField txtUsuarioIniciarSesion;
+
+    
+    
+    @FXML
+    void btnIngresarAdmin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnIniciarSesion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRegistrarUsuario(ActionEvent event) {
+
+    }
+
+}
