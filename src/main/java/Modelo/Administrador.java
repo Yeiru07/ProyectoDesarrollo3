feca7cca836ee8b572fb9996aa0c377269749b63
russe <russe@10.153.159.1>
2026-05-31 19:17:44 -0600

    Conexion de vistas

--- src/main/java/Modelo/Administrador.java
+++ src/main/java/Modelo/Administrador.java
@@ -1,13 +1,19 @@
 package Modelo;
 
+import java.util.ArrayList;
+
 public class Administrador implements Interfaces.ItipoUsuario {
 
     private String nombreDelAdministrador;
+private ArrayList<Sala> arregloDeSalas;
 
-    public Administrador(String nombreDelAdministrador) {
+    public Administrador(String nombreDelAdministrador, ArrayList<Sala> arregloDeSalas) {
         this.nombreDelAdministrador = nombreDelAdministrador;
+        this.arregloDeSalas = arregloDeSalas;
     }
-    
+
+   
+
     @Override
     public String getTipo() {
         return "Administrador";
