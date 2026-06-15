810d66ccab5c7061788412d7d1f82b06126aa088
russe <russe@10.153.159.1>
2026-06-14 19:16:22 -0600

    Ahora presentan preguntas

--- src/main/java/Controlador/vista/VistaPreguntaMultipleController.java
+++ src/main/java/Controlador/vista/VistaPreguntaMultipleController.java
@@ -46,75 +46,33 @@ public class VistaPreguntaMultipleController
                 "PREGUNTAS CARGADAS = "
                 + App.preguntasActuales.size()
         );
-
         if (!App.preguntasActuales.isEmpty()) {
-
-            Preguntas pregunta
-                    = App.preguntasActuales.get(0);
-
-            lblEnunciado.setText(
-                    pregunta.getEnunciado()
-            );
-
-            btnOpcion1.setText(
-                    pregunta
-                            .getArregloDeRespuestasParaPreguntas()
-                            .get(0)
-                            .getRespuestas()
-            );
-
-            btnOpcion2.setText(
-                    pregunta
-                            .getArregloDeRespuestasParaPreguntas()
-                            .get(1)
-                            .getRespuestas()
-            );
-
-            btnOpcion3.setText(
-                    pregunta
-                            .getArregloDeRespuestasParaPreguntas()
-                            .get(2)
-                            .getRespuestas()
-            );
-
-            btnOpcion4.setText(
-                    pregunta
-                            .getArregloDeRespuestasParaPreguntas()
-                            .get(3)
-                            .getRespuestas()
-            );
+            Preguntas pregunta = App.preguntasActuales.get(0);
+            lblEnunciado.setText(pregunta.getEnunciado());
+            btnOpcion1.setText(pregunta.getArregloDeRespuestasParaPreguntas().get(0).getRespuestas());
+            btnOpcion2.setText(pregunta.getArregloDeRespuestasParaPreguntas().get(1).getRespuestas());
+            btnOpcion3.setText(pregunta.getArregloDeRespuestasParaPreguntas().get(2).getRespuestas());
+            btnOpcion4.setText(pregunta.getArregloDeRespuestasParaPreguntas().get(3).getRespuestas());
         }
     }
 
     @FXML
     private void onResponderOpcion1() {
-        lblEstadoSeleccion.setText(
-                "Seleccionaste: "
-                + btnOpcion1.getText()
-        );
+        lblEstadoSeleccion.setText("Seleccionaste: " + btnOpcion1.getText());
     }
 
     @FXML
     private void onResponderOpcion2() {
-        lblEstadoSeleccion.setText(
-                "Seleccionaste: "
-                + btnOpcion2.getText()
-        );
+        lblEstadoSeleccion.setText("Seleccionaste: " + btnOpcion2.getText());
     }
 
     @FXML
     private void onResponderOpcion3() {
-        lblEstadoSeleccion.setText(
-                "Seleccionaste: "
-                + btnOpcion3.getText()
-        );
+        lblEstadoSeleccion.setText("Seleccionaste: " + btnOpcion3.getText());
     }
 
     @FXML
     private void onResponderOpcion4() {
-        lblEstadoSeleccion.setText(
-                "Seleccionaste: "
-                + btnOpcion4.getText()
-        );
+        lblEstadoSeleccion.setText("Seleccionaste: " + btnOpcion4.getText());
     }
 }
